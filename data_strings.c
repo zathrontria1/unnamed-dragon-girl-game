e8b90d16053efc40589b3bdf598a4378008c4d30
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-05-06 23:36:10 +0800

    mark fixed strings as const

--- data_strings.c
+++ data_strings.c
@@ -1,14 +1,14 @@
 #include <stdint.h>
 #include "data_strings.h"
 
-uint8_t STR_MSG_TEST_SINGLELINE[] = "The quick brown fox jumps over";
-uint8_t STR_MSG_TEST_MULTILINE[] = "\
+const uint8_t STR_MSG_TEST_SINGLELINE[] = "The quick brown fox jumps over";
+const uint8_t STR_MSG_TEST_MULTILINE[] = "\
 The quick brown fox jumps over\n\
 the lazy dog.`1234567890@#$%^&\n\
 ABCDEFGHIJKLMNOPQRSTUVWXYZ*()_\n\
 Final line! +{}|:\"<>?-=[]\\;',/";
 
-uint8_t STR_MSG_TEST_MULTIPAGE[] = "\
+const uint8_t STR_MSG_TEST_MULTIPAGE[] = "\
 First page of a multi-page\n\
 text box.\r\
 Second page of a multi-page\n\
@@ -18,7 +18,7 @@ text box.\r\
 Final page of a multi-page\n\
 text box.";
 
-uint8_t STR_MSG_TUTORIAL_MP[] = "\
+const uint8_t STR_MSG_TUTORIAL_MP[] = "\
 Welcome to\n\
 Unnamed Dragon Girl Game!\n\
 Press the A Button to read\n\
@@ -45,16 +45,16 @@ and I hope you have fun\n\
 trying the game out.\n\
                  - Zathrontria";
 
-uint8_t STR_MSG_INCOMBAT[] = "Can't use while in combat";
+const uint8_t STR_MSG_INCOMBAT[] = "Can't use while in combat";
 
-uint8_t STR_UI_HELP_MAP[] = "SELECT Button: Return to game";
+const uint8_t STR_UI_HELP_MAP[] = "SELECT Button: Return to game";
 
-uint8_t STR_UI_PLAYERINFO_ML[] = "\
+const uint8_t STR_UI_PLAYERINFO_ML[] = "\
 HP: %u/%u\n\
 ATK: %u DEF: %u\n\
 Money: %u \n\
 Play time:        %6u:%02u:%02u";
 
-uint8_t STR_LEVELNAME_DEBUG_B1F[] = "DEBUG Dungeon B1F";
-uint8_t STR_LEVELNAME_DEBUG_B2F[] = "DEBUG Dungeon B2F";
-uint8_t STR_LEVELNAME_DEBUG_B3F[] = "DEBUG Dungeon B3F";
+const uint8_t STR_LEVELNAME_DEBUG_B1F[] = "DEBUG Dungeon B1F";
+const uint8_t STR_LEVELNAME_DEBUG_B2F[] = "DEBUG Dungeon B2F";
+const uint8_t STR_LEVELNAME_DEBUG_B3F[] = "DEBUG Dungeon B3F";
--- data_strings.h
+++ data_strings.h
@@ -1,15 +1,15 @@
-extern uint8_t STR_MSG_TEST_SINGLELINE[];
-extern uint8_t STR_MSG_TEST_MULTILINE[];
+extern const uint8_t STR_MSG_TEST_SINGLELINE[];
+extern const uint8_t STR_MSG_TEST_MULTILINE[];
 
-extern uint8_t STR_MSG_TEST_MULTIPAGE[];
+extern const uint8_t STR_MSG_TEST_MULTIPAGE[];
 
-extern uint8_t STR_MSG_TUTORIAL_MP[];
+extern const uint8_t STR_MSG_TUTORIAL_MP[];
 
-extern uint8_t STR_MSG_INCOMBAT[];
+extern const uint8_t STR_MSG_INCOMBAT[];
 
-extern uint8_t STR_UI_HELP_MAP[];
-extern uint8_t STR_UI_PLAYERINFO_ML[];
+extern const uint8_t STR_UI_HELP_MAP[];
+extern const uint8_t STR_UI_PLAYERINFO_ML[];
 
-extern uint8_t STR_LEVELNAME_DEBUG_B1F[];
-extern uint8_t STR_LEVELNAME_DEBUG_B2F[];
-extern uint8_t STR_LEVELNAME_DEBUG_B3F[];
+extern const uint8_t STR_LEVELNAME_DEBUG_B1F[];
+extern const uint8_t STR_LEVELNAME_DEBUG_B2F[];
+extern const uint8_t STR_LEVELNAME_DEBUG_B3F[];
