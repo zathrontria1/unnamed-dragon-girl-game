db8a2f7286df308fc7922c866dad6edfb11873ed
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-06-03 00:55:12 +0800

    Separate UI message box functions out for cleanup prep

--- ui.h
+++ ui.h
@@ -35,16 +35,8 @@ void UserInterface_UpdateHealthCounters(void);
 void UserInterface_UpdateMoneyCounters(void);
 void UserInterface_UpdateEnemyCounters(void);
 
-void UserInterface_PrintText_MultiLine(uint8_t * string_ptr, uint16_t row, uint16_t col);
 void UserInterface_PrintText(uint8_t * string_ptr, uint16_t row, uint16_t col);
 void UserInterface_PrintText_Mode3(uint8_t * string_ptr, uint16_t row, uint16_t col);
-
-void UserInterface_ClearText(uint16_t len, uint16_t row, uint16_t col);
-
-void UserInterface_DrawTextbox(uint16_t row, uint16_t h);
-
-void UserInterface_ClearTextbox(uint16_t row, uint16_t h);
-void UserInterface_ClearTextboxText(uint16_t row, uint16_t h);
 
 // Genericized window drawing functions
 void UserInterface_ClearWindowBuffer(bool use_clear_tile);
@@ -56,8 +48,6 @@ void UserInterface_DrawWindowText(char * string_ptr, uint16_t x, uint16_t y);
 
 void UserInterface_CopyUiBuffers();
 void UserInterface_CopyTextBuffer_Line(uint16_t y);
-
-void UserInterface_CopyTextboxToVram(uint16_t row, uint16_t h);
 
 void UserInterface_DrawEnemyHealthBar(struct game_object * o);
 
