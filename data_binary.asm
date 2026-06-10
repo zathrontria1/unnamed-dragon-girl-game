c25528ba42c1660e39ed8d8d6cd5b42540e756cb
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-06-10 22:46:38 +0800

    Revert to old style of setting up binary data as too unreliable

--- data_binary.asm
+++
@@ -1,7 +0,0 @@
-; For VBCC
-    include "data_bg.asm"
-    include "data_ui.asm"
-    include "data_sprite.asm"
-    include "data_palette.asm"
-    include "data_snd.asm"
-    include "data_samples.asm"
