d6959e537a346d458fa296903518f5b16127f23e
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-06-17 21:43:17 +0800

    Split palettes to 32-byte subpalettes

--- data_palette.asm
+++ data_palette.asm
@@ -1,32 +1,56 @@
 ; For VBCC
 
     section "_rodata.far.bindata.palette.maingame.0"
-    global _data_palette
-_data_palette:
-    incbin "palette/palette_ui_fixed_4bpp.bin"
-    incbin "palette/palette_bg_dungeon_0.bin"
-    incbin "palette/palette_bg_dungeon_1.bin"
-    incbin "palette/palette_bg_dungeon_2.bin"
-    incbin "palette/palette_bg_blank.bin"
-    incbin "palette/palette_bg_blank.bin"
-    incbin "palette/palette_bg_blank.bin"
+    global _data_palette_blank
+_data_palette_blank:
     incbin "palette/palette_bg_blank.bin"
 
-    incbin "palette/palette_spr_player.bin"
-    incbin "palette/palette_spr_common0.bin"
-    incbin "palette/palette_spr_common1.bin"
-    incbin "palette/palette_spr_common2.bin"
-    incbin "palette/palette_bg_blank.bin"
-    incbin "palette/palette_bg_blank.bin"
-    incbin "palette/palette_spr_player_portrait.bin"
+    section "_rodata.far.bindata.palette.maingame.1"
+    global _data_palette_ui
+_data_palette_ui:
+    incbin "palette/palette_ui_fixed_4bpp.bin"
+
+    section "_rodata.far.bindata.palette.maingame.2"
+    global _data_palette_dungeon_0
+_data_palette_dungeon_0:
     incbin "palette/palette_bg_dungeon_0.bin"
-    
+
+    section "_rodata.far.bindata.palette.maingame.3"
+    global _data_palette_dungeon_1
+_data_palette_dungeon_1:
+    incbin "palette/palette_bg_dungeon_1.bin"
+
+    section "_rodata.far.bindata.palette.maingame.4"
+    global _data_palette_dungeon_2
+_data_palette_dungeon_2:
+    incbin "palette/palette_bg_dungeon_2.bin"
+
+    section "_rodata.far.bindata.palette.maingame.5"
+    global _data_palette_player
+_data_palette_player:
+    incbin "palette/palette_spr_player.bin"
+
+    section "_rodata.far.bindata.palette.maingame.6"
+    global _data_palette_common_0
+_data_palette_common_0:
+    incbin "palette/palette_spr_common0.bin"
+
+    section "_rodata.far.bindata.palette.maingame.7"
+    global _data_palette_common_1
+_data_palette_common_1:
+    incbin "palette/palette_spr_common1.bin"
+
     section "_rodata.far.bindata.palette.subscreen.0"
+    global _data_palette_player_portrait
+_data_palette_player_portrait:
+    incbin "palette/palette_spr_player_portrait.bin"
+    
+    section "_rodata.far.bindata.palette.subscreen.1"
     global _data_palette_map_0_8bpp
 _data_palette_map_0_8bpp:
     incbin "palette/palette_bg_map_dungeon_0_8bpp.bin"
 
-    section "_rodata.far.bindata.palette.subscreen.1"
+    section "_rodata.far.bindata.palette.subscreen.2"
     global _data_palette_map_1_8bpp
 _data_palette_map_1_8bpp:
     incbin "palette/palette_bg_map_dungeon_1_8bpp.bin"
--- vars_extern.h
+++ vars_extern.h
@@ -7,9 +7,21 @@ extern const struct enemy_data data_enemy_stats_lizardman_archer;
 extern const struct enemy_data data_enemy_stats_lizardman_lilsis;
 
 // Game data (non-audio)
-extern const uint8_t data_palette[];
+extern const uint8_t data_palette_blank[];
+
+extern const uint8_t data_palette_ui[];
+extern const uint8_t data_palette_dungeon_0[];
+extern const uint8_t data_palette_dungeon_1[];
+extern const uint8_t data_palette_dungeon_2[];
+
+extern const uint8_t data_palette_player[];
+extern const uint8_t data_palette_player_portrait[];
+extern const uint8_t data_palette_common_0[];
+extern const uint8_t data_palette_common_1[];
+
 extern const uint8_t data_palette_map_0_8bpp[];
 extern const uint8_t data_palette_map_1_8bpp[];
+
 extern const uint8_t data_palette_splash[];
 
 extern const uint8_t data_bg_dungeon_lz4[]; // dungeon tiles
@@ -42,6 +54,16 @@ extern const uint8_t data_spr_lizardman[]; // lizardman
 extern const uint8_t data_spr_spawn_placeholder[]; // spawning placeholder
 extern const uint8_t data_spr_drop_coin[]; // dropped coins
 
+// Tables
+extern const NEAR int32_t data_sine_1[];
+extern const NEAR int32_t data_cosine_1[];
+extern const NEAR uint32_t data_pow_2[];
+
+extern const uint32_t data_upgrade_costs[];
+
+// Level sub-data
+extern const uint8_t * data_palette_list_0[];
+
 extern const struct obj_list_entry_interactable data_interactables_debug0[];
 extern const struct obj_list_entry_interactable data_interactables_debug1[];
 extern const struct obj_list_entry_interactable data_interactables_debug2[];
@@ -53,12 +75,6 @@ extern const struct obj_list_entry_spawns data_spawnlist_debug2[];
 extern const struct obj_list_entry_spawners data_spawners_debug0[];
 extern const struct obj_list_entry_spawners data_spawners_debug1[];
 extern const struct obj_list_entry_spawners data_spawners_debug2[];
-
-extern const NEAR int32_t data_sine_1[];
-extern const NEAR int32_t data_cosine_1[];
-extern const NEAR uint32_t data_pow_2[];
-
-extern const uint32_t data_upgrade_costs[];
 
 // Level data
 extern const uint8_t data_map_debug0[];
@@ -74,6 +90,8 @@ extern const uint16_t data_map_lut_dungeon[1024];
 extern const uint8_t data_map_lut_dungeon_col[256];
 
 // Metasprite lists
-extern const struct spr_metaspr_definition data_metaspr_door_ns[];
-extern const struct spr_metaspr_definition data_metaspr_door_ew[];
+extern const struct spr_metaspr_definition data_metaspr_door_ns_open[];
+extern const struct spr_metaspr_definition data_metaspr_door_ns_closed[];
+extern const struct spr_metaspr_definition data_metaspr_door_ew_open[];
+extern const struct spr_metaspr_definition data_metaspr_door_ew_closed[];
 
