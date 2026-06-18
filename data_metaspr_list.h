4cd6ea8ef3208ad73814cbbf7c13b5a7c9dd628e
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-06-18 21:44:49 +0800

    Prelim work to implement level warp object

--- data_metaspr_list.h
+++ data_metaspr_list.h
@@ -36,3 +36,10 @@ const struct spr_metaspr_definition data_metaspr_door_ew_open_flip[] = {
     {0x6a | PAL_INTERACTABLE_BLOCKER_DOOR << 9 | 2 << 12 | true << 14, 0, -48, 0},
     {0, 0, 0, 0xffff},
 };
+
+const struct spr_metaspr_definition data_metaspr_level_warp_closed[] = {
+    {0x8c | PAL_INTERACTABLE_BLOCKER_DOOR << 9 | 2 << 12, 0, -16, 1},
+    {0x8a | PAL_INTERACTABLE_BLOCKER_DOOR << 9 | 2 << 12, 0, -32, 0},
+    {0x8a | PAL_INTERACTABLE_BLOCKER_DOOR << 9 | 2 << 12, 16, -32, 0},
+    {0, 0, 0, 0xffff},
+};
