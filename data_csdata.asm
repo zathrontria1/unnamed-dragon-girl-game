f2a712abbe3267b96d9d998a43904fe76688cc8e
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-06-20 21:19:50 +0800

    Split cutscene data into smaller pieces

--- data_csdata.asm
+++ data_csdata.asm
@@ -4,86 +4,102 @@
     global _data_cs_bbb_0_f
 _data_cs_bbb_0_f:
     incbin "cutscene/bigbuckbunny/0.bin"
+    section "_rodata.far.bindata.cs.bbb.1"
     global _data_cs_bbb_0_p
 _data_cs_bbb_0_p:
     incbin "cutscene/bigbuckbunny/0_p.bin"
+    section "_rodata.far.bindata.cs.bbb.2"
     global _data_cs_bbb_0_t
 _data_cs_bbb_0_t:
     incbin "cutscene/bigbuckbunny/0_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.1"
+    section "_rodata.far.bindata.cs.bbb.3"
     global _data_cs_bbb_1_f
 _data_cs_bbb_1_f:
     incbin "cutscene/bigbuckbunny/1.bin"
+    section "_rodata.far.bindata.cs.bbb.4"
     global _data_cs_bbb_1_p
 _data_cs_bbb_1_p:
     incbin "cutscene/bigbuckbunny/1_p.bin"
+    section "_rodata.far.bindata.cs.bbb.5"
     global _data_cs_bbb_1_t
 _data_cs_bbb_1_t:
     incbin "cutscene/bigbuckbunny/1_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.2"
+    section "_rodata.far.bindata.cs.bbb.6"
     global _data_cs_bbb_2_f
 _data_cs_bbb_2_f:
     incbin "cutscene/bigbuckbunny/2.bin"
+    section "_rodata.far.bindata.cs.bbb.7"
     global _data_cs_bbb_2_p
 _data_cs_bbb_2_p:
     incbin "cutscene/bigbuckbunny/2_p.bin"
+    section "_rodata.far.bindata.cs.bbb.8"
     global _data_cs_bbb_2_t
 _data_cs_bbb_2_t:
     incbin "cutscene/bigbuckbunny/2_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.3"
+    section "_rodata.far.bindata.cs.bbb.9"
     global _data_cs_bbb_3_f
 _data_cs_bbb_3_f:
     incbin "cutscene/bigbuckbunny/3.bin"
+    section "_rodata.far.bindata.cs.bbb.10"
     global _data_cs_bbb_3_p
 _data_cs_bbb_3_p:
     incbin "cutscene/bigbuckbunny/3_p.bin"
+    section "_rodata.far.bindata.cs.bbb.11"
     global _data_cs_bbb_3_t
 _data_cs_bbb_3_t:
     incbin "cutscene/bigbuckbunny/3_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.4"
+    section "_rodata.far.bindata.cs.bbb.12"
     global _data_cs_bbb_4_f
 _data_cs_bbb_4_f:
     incbin "cutscene/bigbuckbunny/4.bin"
+    section "_rodata.far.bindata.cs.bbb.13"
     global _data_cs_bbb_4_p
 _data_cs_bbb_4_p:
     incbin "cutscene/bigbuckbunny/4_p.bin"
+    section "_rodata.far.bindata.cs.bbb.14"
     global _data_cs_bbb_4_t
 _data_cs_bbb_4_t:
     incbin "cutscene/bigbuckbunny/4_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.5"
+    section "_rodata.far.bindata.cs.bbb.15"
     global _data_cs_bbb_5_f
 _data_cs_bbb_5_f:
     incbin "cutscene/bigbuckbunny/5.bin"
+    section "_rodata.far.bindata.cs.bbb.16"
     global _data_cs_bbb_5_p
 _data_cs_bbb_5_p:
     incbin "cutscene/bigbuckbunny/5_p.bin"
+    section "_rodata.far.bindata.cs.bbb.17"
     global _data_cs_bbb_5_t
 _data_cs_bbb_5_t:
     incbin "cutscene/bigbuckbunny/5_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.6"
+    section "_rodata.far.bindata.cs.bbb.18"
     global _data_cs_bbb_6_f
 _data_cs_bbb_6_f:
     incbin "cutscene/bigbuckbunny/6.bin"
+    section "_rodata.far.bindata.cs.bbb.19"
     global _data_cs_bbb_6_p
 _data_cs_bbb_6_p:
     incbin "cutscene/bigbuckbunny/6_p.bin"
+    section "_rodata.far.bindata.cs.bbb.20"
     global _data_cs_bbb_6_t
 _data_cs_bbb_6_t:
     incbin "cutscene/bigbuckbunny/6_t.bin"
 
-    section "_rodata.far.bindata.cs.bbb.7"
+    section "_rodata.far.bindata.cs.bbb.21"
     global _data_cs_bbb_7_f
 _data_cs_bbb_7_f:
     incbin "cutscene/bigbuckbunny/7.bin"
+    section "_rodata.far.bindata.cs.bbb.22"
     global _data_cs_bbb_7_p
 _data_cs_bbb_7_p:
     incbin "cutscene/bigbuckbunny/7_p.bin"
+    section "_rodata.far.bindata.cs.bbb.23"
     global _data_cs_bbb_7_t
 _data_cs_bbb_7_t:
     incbin "cutscene/bigbuckbunny/7_t.bin"
