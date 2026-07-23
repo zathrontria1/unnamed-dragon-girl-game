ba912bf6d09acb3823da7c7d8cb66b6dcb503618
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-07-23 22:57:29 +0800

    Fix misnomer

--- src/graphics/ani_fixedspr.c
+++ src/graphics/ani_fixedspr.c
@@ -8,8 +8,8 @@
 #include "ani_fixedspr.h"
 #include "dma.h"
 
-uint16_t ani_bg_frame_coin;
-uint8_t * ani_bg_addr_coin;
+uint16_t ani_fixedspr_frame_coin;
+uint8_t * ani_fixedspr_addr_coin;
 
 bool ani_coin_flip; // Used by the coin function to enable H flip.
 
@@ -20,24 +20,24 @@ void AniSystem_Spr_UpdateFixedTiles()
 {
     if (!((uint16_t)system_frames_elapsed & ANI_INTERVAL_8))
     {
-        ani_bg_frame_coin++;
+        ani_fixedspr_frame_coin++;
     }
 
     uint16_t temp_frame;
 
-    if (ani_bg_frame_coin >= 11)
+    if (ani_fixedspr_frame_coin >= 11)
     {
-        ani_bg_frame_coin = 0;
+        ani_fixedspr_frame_coin = 0;
         temp_frame = 0;
     }
-    else if (ani_bg_frame_coin >= 6)
+    else if (ani_fixedspr_frame_coin >= 6)
     {
-        temp_frame = 11 - (ani_bg_frame_coin + 1);
+        temp_frame = 11 - (ani_fixedspr_frame_coin + 1);
         ani_coin_flip = true;
     }
     else
     {
-        temp_frame = ani_bg_frame_coin;
+        temp_frame = ani_fixedspr_frame_coin;
         ani_coin_flip = false;
     }
 
@@ -45,11 +45,11 @@ void AniSystem_Spr_UpdateFixedTiles()
     uint8_t * temp_src = ((uint8_t *)&data_spr_drop_coin) + (temp_frame << 6); // 64 bytes
     uint16_t temp_dest = 0x62a0;
 
-    if (temp_src != ani_bg_addr_coin)
+    if (temp_src != ani_fixedspr_addr_coin)
     {
         if (!DmaSystem_AddItemToQueue(temp_src, temp_dest, 128, VRAM_INCHIGH, 1))
         {
-            ani_bg_addr_coin = temp_src;
+            ani_fixedspr_addr_coin = temp_src;
         }
     }
 
--- src/graphics/ani_fixedspr.h
+++ src/graphics/ani_fixedspr.h
@@ -1,5 +1,5 @@
-extern uint16_t ani_bg_frame_coin;
-extern uint8_t * ani_bg_addr_coin;
+extern uint16_t ani_fixedspr_frame_coin;
+extern uint8_t * ani_fixedspr_addr_coin;
 extern bool ani_coin_flip;
 
 void AniSystem_Spr_UpdateFixedTiles(void);
