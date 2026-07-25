675673f7ae7b35498aea2428954efe371b41414f
zathrontria1 <56213382+zathrontria1@users.noreply.github.com>
2026-07-25 19:56:47 +0800

    Add coin animation on opening treasure chest

--- src/gameplay/routines.c
+++ src/gameplay/routines.c
@@ -227,6 +227,39 @@ void Routines_TreasureChest(struct game_object * o)
             else
             {
                 // Decrement time to live
+
+                // Also draw a coin on top of it
+                // Make a copy of the object
+                game_object_t c = *o;
+
+                if (o->struct_data.interactable_data.ttl >= 90 / V_MUL)
+                {
+                    // Calculate elapsed frames since chest was opened
+                    int elapsed = (180 / V_MUL) - o->struct_data.interactable_data.ttl;
+
+                    // Simulate Z-axis gravity drop/bounce physics matching AniSystem_AnimateDropGravity
+                    int32_t bounce_z = 0;
+                    int32_t dz = (3 * V_S_ONE);
+
+                    for (int i = 0; i < elapsed; i++)
+                    {
+                        bounce_z += dz;
+                        dz -= (V_GRAVITY >> 1);
+
+                        if (bounce_z <= 0)
+                        {
+                            bounce_z = 0;
+                            dz = 0;
+                            break;
+                        }
+                    }
+
+                    // Apply resting height offset (+16px above chest origin) plus bounce height
+                    c.pos.z.a += (8l << 16) + bounce_z;
+
+                    Routines_Shared_DrawFixed(&c, 0x2a | PAL_DROP_MONEY << 9, 0, false);
+                } // Don't draw the coin if less than 90 / V_MUL frames left
+
                 o->struct_data.interactable_data.ttl--;
             }
         }
